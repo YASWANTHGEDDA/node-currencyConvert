b230e8753af8735645392ce65d7ef270ef7684f4
YASWANTHGEDDA <yaswanth.gedda@sasi.ac.in>
2025-02-05 21:33:58 +0530

    modify server

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = 2039;
 
 app.use('/',(req,res)=> {
   res.sendFile(path.join(__dirname,'index.html'));
-  console.log(`request serverd by the ${replica} app`);
+  console.log(`request serverd by the  app`);
   
 });
 
 app.listen(port,() =>{
-  console.log(`${replica} is listening on the port:${port}`);
+  console.log(` is listening on the port:${port}`);
   
 })
